0b2cc751589b95c457b18b65bdae53ae343f41cd
anderson <anderson_seibert@hotmail.com>
2017-09-21 20:44:26 -0300

    No commit message

---
+++ tcp_client_02_threaded.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8  -*-
+import socket
+import threading;
+
+terminate = False
+
+
+class TCPReader(threading.Thread):
+    def __init__(self, client_socket):
+        threading.Thread.__init__(self)
+        self.socket = client_socket
+
+    def run(self):
+        global terminate
+        while not terminate:
+            data = self.socket.recv(256)
+            print data
+
+
+class KBReader(threading.Thread):
+    def __init__(self, client_socket):
+        threading.Thread.__init__(self)
+        self.socket = client_socket
+
+    def run(self):
+        global terminate
+        while not terminate:
+            line = raw_input().strip()
+
+            if line == "bye":
+                terminate = True
+
+            self.socket.send(line.strip())
+
+
+def main():
+
+    #Criação do socket TCP
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # solicita conexão
+    c.connect(("127.0.0.1", 8000))
+
+    th1 = TCPReader(c)
+    th2 = KBReader(c)
+
+    th1.start()
+    th2.start()
+
+    th1.join()
+    th2.join()
+
+
+if __name__ == "__main__":
+        main()
