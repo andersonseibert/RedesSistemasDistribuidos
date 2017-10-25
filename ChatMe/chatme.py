1362d38028d9bd12b6ed6f11b5eb50e4cba48770
Anderson Seibert <anderson_seibert@hotmail.com>
2017-10-25 20:00:05 -0200

    No Commit Message

--- ChatMe/chatme.py
+++ ChatMe/chatme.py
@@ -8,13 +8,13 @@ from threading import Thread
 broadcast = "255.255.255.255"
 port = 6777
 voto_instancia = uuid.uuid4()
-dados = "{remetente: Anderson, tipo:0, voto_instancia: 33, voto_distribuidor: 0}"
+dados = "{remetente: "+voto_instancia.hex+", tipo:0, voto_instancia: 33, voto_distribuidor: 0}"
 
 
 def sendmessage(msg, repeat):  # ENVIA MENSAGEM
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    #for i in range(1, repeat):
+    # for i in range(1, repeat):
     while 1:
         sock.sendto(msg, (broadcast, port))
         sleep(0.2)  # intervalo de tempo de envio de pacotes
@@ -25,11 +25,28 @@ def sendbroadcast(msg, repeat):
     sendmessage(msg, ("255.255.255.255", 6777), repeat)
 
 
+# listener
+def broadcast_listener():
+    global port
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind('0.0.0.0', port)
+
+    while True:
+        data = sock.recv(1024)
+        if not data:
+            break
+
+        print data + "\n"
+
+
 # funcao main
 def main():
     thread_announcer = Thread(target=sendmessage(""+dados, 4))
+    thread_broadcast_listener = Thread(target=broadcast_listener)
+    thread_broadcast_listener.start()
     thread_announcer.start()
-    # sendmessage("ChupaCabra", 4)
 
 
 if __name__ == '__main__':
